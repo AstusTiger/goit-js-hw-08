9eecbfe90a289c78e4100c540a79c8f67bda1c67
AstusTiger <astus.tiger@gmail.com>
2022-11-04 18:39:25 +0200

    gallery done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,22 @@
 // Add imports above this line
-import SimpleLightbox from 'simplelightbox';
+import SimpleLightbox from 'simplelightbox/src/simple-lightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkUp = galleryItems.reduce((acc, item) => {
+  return (
+    acc +
+    `<a class='gallery__item' href=${item.original}><img class='gallery__image' src=${item.preview} alt=${item.description}/></a>`
+  );
+}, '');
+gallery.insertAdjacentHTML('afterbegin', galleryMarkUp);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+});
