fc342748eb3a087aed4a8024e66c1c03ba76b18f
AstusTiger <astus.tiger@gmail.com>
2022-11-04 20:53:55 +0200

    try4

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
 import Player from '@vimeo/player';
+
+var throttle = require('lodash.throttle');
+
+console.log(1);
+
+//const iframe = document.querySelector('#vimeo-player');
+const player = new Player('vimeo-player', {
+  id: 19231868,
+  width: 640,
+});
+
+player.setCurrentTime(
+  window.localStorage.getItem('CurrentTime')
+    ? window.localStorage.getItem('CurrentTime')
+    : 0
+);
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(data => {
+      console.log(data);
+      window.localStorage.setItem('CurrentTime', data);
+    });
+  }, 1000)
+);
+
+// const intervalId = setInterval(
+//   () =>
+//     player.getCurrentTime().then(data => {
+//       console.log(data);
+//       window.localStorage.setItem('CurrentTime', data);
+//     }),
+//   1000
+// );
+
+console.log(player);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+var throttle = require('lodash.throttle');
 
+const form = document.querySelector('.feedback-form');
+const input = form.querySelector('input[name="email"]');
+const textArea = form.querySelector('textarea[name="message"]');
+
+input.value =
+  JSON.parse(localStorage.getItem('feedback-form-state'))?.email || '';
+textArea.value =
+  JSON.parse(localStorage.getItem('feedback-form-state'))?.message || '';
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log({ email: input.value, message: textArea.value });
+  localStorage.removeItem('feedback-form-state');
+  input.value = '';
+  textArea.value = '';
+  console.log('CLICK');
+});
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    console.log(input.value);
+    console.log(textArea.value);
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify({
+        email: input.value,
+        message: textArea.value,
+      })
+    );
+  }, 500)
+);
